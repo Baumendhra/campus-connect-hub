cc1bb7f94a2a92cf04060ee6a0f60b6eeab849c7
baumendhra <baumendhra@gmail.com>
2026-03-25 00:09:27 +0530

    checking...

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -7,7 +7,7 @@ import { useEffect } from 'react';
 export default function DashboardPage() {
   const { profile } = useAuth();
 
-  if(!profile) {
+  if(profile) {
     window.location.href = "/";
     return null;
   }
